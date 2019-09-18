521ef60f1dc357a6ae56024c38e238f825932e53
lily-shen <921180142@qq.com>
2019-09-18 20:23:26 +0800

    Signed-off-by: lily-shen <921180142@qq.com>

--- js/main.js
+++ js/main.js
@@ -4,29 +4,15 @@ $(function () {
     $(this).html(response);
   });
 
+
   //加载footer
   $('footer').load("footer.html", "", function (response, status, request) {
     $(this).html(response);
   });
 
-  // 搜索框弹出
-  var isshowSearch = false
-  $("header").delegate("#search_btn", "click", function () {
-    $('.nav-show').css({ 'opacity': 0 })
-    $('.search-form').addClass('search-show')
-    $('.header').addClass('bgf')
-    isshowSearch  = true
-  });
-  // 搜索框g关闭
-  $("header").delegate(".search-close", "click", function () {
-    $('.nav-show').css({ 'opacity': 1 })
-    $('.search-form').removeClass('search-show')
-    isshowSearch  = false
-  });
-
   $(window).scroll(function () {
     var top = $(window).scrollTop();
-    if (top >= 100 || isshowSearch== true ) {
+    if (top >= 100) {
       $('.header').addClass('bgf')
     } else {
       $('.header').removeClass('bgf')
